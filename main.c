ff98b9c0a3523f6e4f5d5e87e7e3a6667c6ba5f6
0701MF20 <72437159+0701MF20@users.noreply.github.com>
2021-09-20 11:04:46 +0530

    check whether any element in array is duplicate
    c program

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,item,noe=5,index;
+   int i,j,item,noe=5,index,y=-1;
     int A[6];
     printf("Entered the element in array:-\n");
     for(i=0;i<noe;i++)
@@ -10,25 +10,21 @@ int main()
         printf("Element entered at the index %d is ",i);
         scanf("%d",&A[i]);
     }
-    printf("Enter the element to be deleted in array :");
-    scanf("%d",&item);
-    for(i=0;i<noe;i++)
-    {
-       if(A[i]==item)
-        index=i;
-    }
-    for(i=index;i<noe;i++)
-    {
-        A[i]=A[i+1];
-    }
-    printf("Displat of array: ");
     for(i=0;i<noe-1;i++)
     {
-      printf("%d",A[i]);
-      if(i==0||i<noe-2)
+      for(j=1+i;j<noe;j++)
       {
-      printf(",");
+        if(A[i]==A[j])
+        {
+            y=1;
+            printf("Duplicate element in the array is %d\n",A[i]);
+            break;
+        }
       }
+    }
+    if(y==-1)
+    {
+      printf("No duplicate element is present in sorted array");
     }
     getch();
 }
