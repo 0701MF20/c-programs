8a044b90761e5ea13c675754cb87d4c80b18eca2
0701MF20 <72437159+0701MF20@users.noreply.github.com>
2021-09-20 10:35:57 +0530

    structure of complex number

---
+++ im.c
@@ -0,0 +1,17 @@
+#include<iostream>
+//#include<process.h>
+#include<cstdio>
+//#include<conio.h>
+using namespace std;
+struct complex{
+int real;
+int imag;
+};
+int main()
+{
+ struct complex r;
+ r.imag=4;
+ r.real=8;
+cout<<"the complex no is"<<r.real;
+}
+
