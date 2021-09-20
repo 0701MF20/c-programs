bf71775727e550aa0a27cb8a9929daa9110d2ecc
0701MF20 <72437159+0701MF20@users.noreply.github.com>
2021-09-20 10:14:28 +0530

    Summation of salary(Simple addition) c program

--- main.c
+++ main.c
@@ -1,90 +1,37 @@
-/*#include<stdio.h>
-int factorial(int n)
-{
-    int i;
-    int fact=1;
-    for(i=2;i<=n;i++)
-    {
-        fact=fact*i;
-    }
-    return fact;
-}
+#include <stdio.h>
 int main()
 {
-    int n;
-    printf("Enter the value of n :");
-    scanf("%d",&n);
-    printf("Factorial of n :%d",factorial(n));
-    getch();
-}
-*/
-/*
-#include<stdio.h>
-int factorial(int n)
+int A[5][3],sum,i,j;
+printf("Enter the record:-\n");
+for(i=0;i<5;i++)
 {
-    if(n==1)
-    return 1;
-    else
+    printf("Enter the sales of 3 item by salesman %d is :-\n",i+1);
+    for(j=0;j<3;j++)
     {
-    if(n>1)
-    {
-        return factorial(n-1)*n;
-    }
+    scanf("%d",&A[i][j]);
     }
 }
-int main()
+for(i=0;i<5;i++)
 {
-    int n;
-    printf("Enter the value of n :");
-    scanf("%d",&n);
-    printf("Factorial of n is :%d",factorial(n));
-    getch();
-}
-*/
-/*
-#include<stdio.h>
-int fib(int n)
-{
-    if(n==1||n==0)
-    return n;
-    else
+    sum=0;
+    for(j=0;j<3;j++)
     {
-        return fib(n-1)+fib(n-2);
+    sum=sum+A[i][j];
     }
+    printf("Total sales of salesman %d is %d",i+1,sum);
+    printf("\n");
+}
+
+for(j=0;j<3;j++)
+{
+    sum=0;
+    for(i=0;i<5;i++)
+    {
+        sum=sum+A[i][j];
+    }
+     printf("Total sales of item %d is %d",j+1,sum);
+     printf("\n");
 
 }
-int main()
-{
-  int n;
-  printf("Enter the value of n :");
-  scanf("%d",&n);
-  for(int i=0;i<n;i++)
-  {
-  printf("%d",fib(i));
-  if(i<(n-1))
-  {
-      printf(",");
-  }
-  }
-  getch();
+getch();
 }
-*/
-/*#include<stdio.h>
-int main()
-{
-    int *c,*d,a,b,temp;
-    printf("Enter the value of a:");
-    scanf("%d",&a);
-    printf("Enter the value of b:");
-    scanf("%d",&b);
-    c=&a;
-    d=&b;
-    temp=*c;
-    *c=*d;
-    *d=temp;
-    printf("Value a and b obtained after swapping:-");
-    printf("\nValue of a :%d",a);
-    printf("\nValue of b :%d",b);
-    getch();
-}
-*/
