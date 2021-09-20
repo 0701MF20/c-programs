ccb1bb2c6179c254141ec885bf806a3a7a25ba09
0701MF20 <72437159+0701MF20@users.noreply.github.com>
2021-09-20 11:00:24 +0530

    enter element in an array at a index
    index is  provided by user

--- main.c
+++ main.c
@@ -2,58 +2,29 @@
 #include<stdio.h>
 int main()
 {
-    int A[5],i,item,ub=4,lb=0,mid,x;
-    printf("Enter 1 for ascending order and 2 for descending order :");
-    scanf("%d",&x);
-    printf("Enter the element in the array :-\n");
-    for(i=0;i<5;i++)
+    int i,index,item,noe=6;
+    int A[7];
+    printf("Entered the element in array:-\n");
+    for(i=0;i<noe;i++)
     {
         printf("Element entered at the index %d is ",i);
         scanf("%d",&A[i]);
     }
-    printf("Enter the elements to be searched: ");
-    scanf("\n%d",&item);
-    if(x==1)
+    printf("Enter the element to be inserted in array :");
+    scanf("%d",&item);
+    printf("Enter the index at which element is added in the array :");
+    scanf("%d",&index);
+    for(i=noe-1;i>=index;i--)
     {
-    for(i=0;i<5;i++)
-    {
-        if(ub>=lb)
-        {
-            mid=(lb+ub)/2;
-            if(A[mid]==item)
-            {
-             printf("Element found at the index: %d",mid);
-             break;
-            }
-            else if(A[i]<item)
-            lb=mid+1;
-            else
-            ub=mid-1;
-        }
+        A[i+1]=A[i];
     }
-    if(A[mid]!=item)
-    printf("Element not found in the array");
-    }
-    else
+    A[index]=item;
+    printf("Display of array after inserting the element in the given index :");
+    for(i=0;i<7;i++)
     {
-         for(i=0;i<5;i++)
-    {
-        if(ub>=lb)
-        {
-            mid=(lb+ub)/2;
-            if(A[mid]==item)
-            {
-             printf("Element found at the index: %d",mid);
-             break;
-            }
-            else if(A[i]>item)
-            lb=mid+1;
-            else
-            ub=mid-1;
-        }
-    }
-    if(A[mid]!=item)
-    printf("Element not found in the array");
+        printf("%d",A[i]);
+        if(i==0||i<6)
+        printf(",");
     }
     getch();
 }
