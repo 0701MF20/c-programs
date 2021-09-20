f07aa6780ef4ad6ed6ab2b3dc02b68f32edceca2
0701MF20 <72437159+0701MF20@users.noreply.github.com>
2021-09-20 10:16:19 +0530

    factorial
    c program of factorial

--- main.c
+++ main.c
@@ -1,66 +1,22 @@
+//25. WAP to compute factorial of numbers using function.
 #include<stdio.h>
+int factorial(int n)
+{
+    int i;
+    int fact=1;
+    for(i=2;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    return fact;
+}
 int main()
 {
-    int A[6][6],B[6][6],C[6][6],i,j,m,n;
-    printf("Enter the value of m :");
-    scanf("%d",&m);
+    int n;
+    printf("Enter the data:-\n");
     printf("Enter the value of n :");
     scanf("%d",&n);
-        printf("Enter the data in matrix A:-\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-       {
-            scanf("%d",&A[i][j]);
-       }
-    }
-    printf("Display the matrix A:-\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            printf("%d",A[i][j]);
-            printf(" ");
-        }
-        printf("\n");
-    }
-
-        printf("Enter the data in matrix B:-\n");
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                scanf("%d",&B[i][j]);
-            }
-
-        }
-        printf("Display the matrix B:-\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            printf("%d",B[i][j]);
-            printf(" ");
-        }
-        printf("\n");
-    }
-
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-          C[i][j]=A[i][j]+B[i][j];
-        }
-    }
-    printf("Display of sum of matrix A and B:-\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            printf("%d",C[i][j]);
-            printf(" ");
-        }
-        printf("\n");
-    }
+    printf("Factorial of %d :%d",n,factorial(n));
     getch();
 }
+
